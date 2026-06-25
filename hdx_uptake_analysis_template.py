16fa029746311b8e0b88601ab127ef5c3b8f3713
rinkyparakra <rinky.parakra@gmail.com>
2026-06-25 14:43:07 -0400

    Add files via upload

---
+++ hdx_uptake_analysis_template.py
@@ -0,0 +1,5 @@
+peptide,start,end,time_min,apo_mean,bound_mean,delta_bound_minus_apo,interpretation
+AAEASKKPR,101,109,0.5,0.845,0.640,-0.205,Protection
+KRTATKAYN,110,118,0.5,1.140,1.305,0.165,Deprotection
+AAEASKKPR,101,109,5,2.150,1.580,-0.570,Protection
+KRTATKAYN,110,118,5,2.760,3.050,0.290,Deprotection
